46b053dff20afa243f8d7d783a955a0044076e57
Frank Wagner <frank2568@gmail.com>
2024-01-23 21:26:02 +0100

    added GeneIdentifier to Model

---
+++ src/Eryph.GenePool.Model/GeneType.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Eryph.GenePool.Model;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum GeneType
+{
+    Catlet,
+    Volume,
+    Fodder
+}
