7f3b75412f487d665ab7b5579a48daf9a36b563d
Frank Wagner <frank2568@gmail.com>
2024-01-25 00:34:10 +0100

    added shared serializer options

---
+++ src/Eryph.GenePool.Model/GeneModelDefaults.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Eryph.GenePool.Model
+{
+    public class GeneModelDefaults
+    {
+        private static JsonSerializerOptions? _options;
+
+        public static JsonSerializerOptions SerializerOptions
+        {
+            get
+            {
+                if (_options != null) return _options;
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+                };
+                _options = options;
+
+                return _options;
+            }
+        }
+    }
+}
