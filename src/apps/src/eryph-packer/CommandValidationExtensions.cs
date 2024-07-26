a2e1ff7dbd7f19696c5013d9bc05acee2512d636
Frank Wagner <frank2568@gmail.com>
2024-07-26 10:59:37 +0200

    added validation to command parsing

---
+++ src/apps/src/eryph-packer/CommandValidationExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.CommandLine;
+using Eryph.GenePool.Client.Internal;
+using LanguageExt;
+using LanguageExt.Common;
+
+public static class CommandValidationExtensions
+{
+    public static Argument<T> AddValidation<T,TValidation>(
+        this Argument<T> argument,
+        Func<T, Validation<Error, TValidation>> validator)
+    {
+
+        argument.AddValidator(result =>
+        {
+            var value = result.GetValueOrDefault<T>();
+            var validation = validator(value);
+            var either = validation.ToEither().MapLeft(Error.Many).Map(_ => value);
+            either.IfLeft(l => result.ErrorMessage = $"Invalid argument <{argument.Name}>: {l.Message}");
+
+        } );
+
+        return argument;
+    }
+
+    public static System.CommandLine.Option<T> AddValidation<T, TValidation>(
+        this System.CommandLine.Option<T> option,
+        Func<T, Validation<Error, TValidation>> validator)
+    {
+
+        option.AddValidator(result =>
+        {
+            var value = result.GetValueOrDefault<T>();
+
+            if (value == null || value.Equals(default(T)))
+                return;
+
+            var validation = validator(value);
+            var either = validation.ToEither().MapLeft(Error.Many).Map(_ => value);
+            either.IfLeft(l => result.ErrorMessage = $"Invalid option {option.Name}: {l.Message}");
+
+        });
+
+        return option;
+    }
+}
