90eab3c82768b55a97ef078f68526842f584716d
Christopher Mann <christopher.mann@dbosoft.eu>
2024-07-26 17:56:25 +0200

    Add validating type for the API key name

---
+++ src/Eryph.GenePool.Model/ApiKeyName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dbosoft.Functional.DataTypes;
+using Eryph.ConfigModel;
+using LanguageExt.ClassInstances;
+
+namespace Eryph.GenePool.Model;
+
+public class ApiKeyName : ValidatingNewType<ApiKeyName, string, OrdStringOrdinalIgnoreCase>
+{
+    public ApiKeyName(string value) : base(value)
+    {
+        ValidOrThrow(Validations<ApiKeyName>.ValidateCharacters(
+                         value,
+                         allowHyphens: true,
+                         allowUnderscores: true,
+                         allowDots: false,
+                         allowSpaces: false)
+                     | Validations<ApiKeyName>.ValidateLength(value, 1, 50));
+    }
+}
