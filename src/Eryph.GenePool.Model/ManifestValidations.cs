fa234da686f581daf5416e1d0774e6ac38de9fc1
Frank Wagner <frank2568@gmail.com>
2024-10-17 11:42:31 +0200

    undo gene hash change....

--- src/Eryph.GenePool.Model/ManifestValidations.cs
+++ src/Eryph.GenePool.Model/ManifestValidations.cs
@@ -66,9 +66,13 @@ public class ManifestValidations
             let splitHash = hash.Split(':')
             from gSep in guard(splitHash.Length == 2,
                 Validations.BadRequestError("Gene hash has to contain one : as hash type separator")).ToValidation()
-            from gType in guard(splitHash[0] is "sha1",
-                Validations.BadRequestError("hash type has to be sha1")).ToValidation()
-            from hashLength in HashSha1.NewValidation(splitHash[1]).Map(_ => Unit.Default)
+            from gType in guard(splitHash[0] is "sha1" or "sha256",
+                Validations.BadRequestError("hash type has to be sha1 or sha256")).ToValidation()
+            from hashLength in
+                splitHash[0] == "sha1"
+                    ? HashSha1.NewValidation(splitHash[1]).Map(_ => Unit.Default)
+                    : HashSha256.NewValidation(splitHash[1]).Map(_ => Unit.Default)
+            
 
             select Unit.Default;
     }
