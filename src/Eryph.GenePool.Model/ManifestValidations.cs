2feb5ba3ca5f78b8aa431e617141b49957671c0a
Frank Wagner <frank2568@gmail.com>
2024-10-17 12:02:13 +0200

    split hash reference validation from gene hash validation

--- src/Eryph.GenePool.Model/ManifestValidations.cs
+++ src/Eryph.GenePool.Model/ManifestValidations.cs
@@ -17,7 +17,7 @@ public class ManifestValidations
                | ValidateProperty(manifest, m=>m.Geneset, GeneSetIdentifier.NewValidation, path, true)
                | ValidateProperty(manifest, m => m.Reference, GeneSetIdentifier.NewValidation, path)
                | ValidateProperty(manifest, m => m.Parent, GeneSetIdentifier.NewValidation, path)
-               | ValidateProperty(manifest, m=>m.CatletGene, ValidateGeneHash, path)
+               | ValidateProperty(manifest, m=>m.CatletGene, ValidateGeneReferenceProperty, path)
                | ValidateList(manifest, m=>m.VolumeGenes, ValidateGeneReference, path, 0, 100)
                | ValidateList(manifest, m=>m.FodderGenes, ValidateGeneReference, path,0, 100)
                | ValidateProperty(manifest, m=> m.Metadata, Validations.ValidateGenesetTagMetadata, path);
@@ -48,7 +48,7 @@ public class ManifestValidations
     {
         return
             ValidateProperty(reference, x => x.Name, GeneName.NewValidation, path, required: true)
-            | ValidateProperty(reference, x => x.Hash, ValidateGeneHash, path, required: true)
+            | ValidateProperty(reference, x => x.Hash, ValidateReferenceHash, path, required: true)
             | ValidateProperty(reference, x => x.Architecture, ValidateArchitecture, path, required: true);
 
 
@@ -59,6 +59,11 @@ public class ManifestValidations
         return ValidateGeneHash(hash).MapFail(e => new ValidationIssue(path, e.Message));
     }
 
+    public static Validation<ValidationIssue, Unit> ValidateGeneReferenceProperty(string hash, string path)
+    {
+        return ValidateReferenceHash(hash).MapFail(e => new ValidationIssue(path, e.Message));
+    }
+
     public static Validation<Error, Unit> ValidateGeneHash(string hash)
     {
         return
@@ -66,13 +71,28 @@ public class ManifestValidations
             let splitHash = hash.Split(':')
             from gSep in guard(splitHash.Length == 2,
                 Validations.BadRequestError("Gene hash has to contain one : as hash type separator")).ToValidation()
+            from gType in guard(splitHash[0] is "sha1",
+                Validations.BadRequestError("hash type has to be sha1")).ToValidation()
+            from hashValid in HashSha1.NewValidation(splitHash[1]).Map(_ => Unit.Default)
+            
+
+            select Unit.Default;
+    }
+
+    public static Validation<Error, Unit> ValidateReferenceHash(string hash)
+    {
+        return
+            from gNull in guard(notEmpty(hash), Validations.BadRequestError("Gene reference hash is empty")).ToValidation()
+            let splitHash = hash.Split(':')
+            from gSep in guard(splitHash.Length == 2,
+                Validations.BadRequestError("Gene reference hash has to contain one : as hash type separator")).ToValidation()
             from gType in guard(splitHash[0] is "sha1" or "sha256",
                 Validations.BadRequestError("hash type has to be sha1 or sha256")).ToValidation()
-            from hashLength in
+            from hashValid in
                 splitHash[0] == "sha1"
                     ? HashSha1.NewValidation(splitHash[1]).Map(_ => Unit.Default)
                     : HashSha256.NewValidation(splitHash[1]).Map(_ => Unit.Default)
-            
+
 
             select Unit.Default;
     }
