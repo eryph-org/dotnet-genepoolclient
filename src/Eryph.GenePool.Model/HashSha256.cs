a10db0b55db2ffa6171d3ba2394237855913a073
Frank Wagner <frank2568@gmail.com>
2024-10-17 08:38:31 +0200

    removed sha256 support for gene hashes this is missleading as genepool only supports sha1

--- src/Eryph.GenePool.Model/HashSha256.cs
+++ src/Eryph.GenePool.Model/HashSha256.cs
@@ -2,17 +2,18 @@
 
 namespace Eryph.GenePool.Model;
 
-public class HashSha256 : EryphName<HashSha256>
-{
-    public HashSha256(string value) : base(value)
-    {
-        ValidOrThrow(Validations<HashSha256>.ValidateCharacters(
-                         value,
-                         allowDots: false,
-                         allowHyphens: false,
-                         allowUnderscores: false,
-                         allowSpaces: false)
-                     | Validations<HashSha256>.ValidateLength(value, 64, 64)
-                     | GeneValidations.ValidateHashCharacters(value));
-    }
-}
+// for future use
+//public class HashSha256 : EryphName<HashSha256>
+//{
+//    public HashSha256(string value) : base(value)
+//    {
+//        ValidOrThrow(Validations<HashSha256>.ValidateCharacters(
+//                         value,
+//                         allowDots: false,
+//                         allowHyphens: false,
+//                         allowUnderscores: false,
+//                         allowSpaces: false)
+//                     | Validations<HashSha256>.ValidateLength(value, 64, 64)
+//                     | GeneValidations.ValidateHashCharacters(value));
+//    }
+//}
